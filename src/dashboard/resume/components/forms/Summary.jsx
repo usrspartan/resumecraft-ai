6a892090b360a7b9ae2fc7d2a62a29dc076e010e
Anupam <mailing2anup@gmail.com>
2024-10-28 11:28:07 +0530

    Gemini API integrated and recieving response from api

--- src/dashboard/resume/components/forms/Summary.jsx
+++ src/dashboard/resume/components/forms/Summary.jsx
@@ -1,18 +1,27 @@
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
 import { ResumeInfoContext } from '@/context/ResumeInfoContext';
-import { LoaderCircle } from 'lucide-react';
+import { LoaderCircle, SparklesIcon } from 'lucide-react';
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import GlobalApi from './../../../../../service/GlobalApi';
 import { toast } from 'sonner';
+import { AIchatSession } from '../../../../../service/AiModal';
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-function Summary({enableNext}) {
+
+
+function Summary({ enableNext }) {
 
     const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
     const [summary, setSummary] = useState("");
     const [loading, setLoading] = useState(false);
     const params = useParams();
+
+    // For Gemini API
+    const prompt = "Job Title = {jobTitle} , Depends on job title give me summary for my resume within 4-5 lines do not use any placeholders";
+
+
 
     // When summary changes
     useEffect(() => {
@@ -21,6 +30,20 @@ function Summary({enableNext}) {
             summery: summary,
         });
     }, [summary]);  // Only run this effect when 'summary' changes
+
+    // Generating Summary from AI
+
+    const GenerateSummaryFromAI = async () => {
+        setLoading(true);
+        const PROMPT = prompt.replace("{jobTitle}", resumeInfo?.jobTitle);
+        console.log("Prompt", PROMPT);
+
+
+        const result = await AIchatSession.sendMessage(PROMPT);
+        console.log("Result of prompt", result.response.text());
+        setLoading(false);
+    }
+
 
     const onSave = (e) => {
         setLoading(true);
@@ -50,7 +73,7 @@ function Summary({enableNext}) {
                 <form className='mt-7' onSubmit={onSave}>
                     <div className='flex justify-between items-end'>
                         <label>Add Summary</label>
-                        <Button variant="outline" size="sm" className="border-primary text-primary">Use AI for summary</Button>
+                        <Button type="button" variant="outline" size="sm" className="border-primary text-primary flex gap-2" onClick={() => GenerateSummaryFromAI()}> <SparklesIcon className='hover:animate-pulse' /> Use AI for summary</Button>
                     </div>
 
                     <Textarea
@@ -61,7 +84,7 @@ function Summary({enableNext}) {
                     />
 
                     <div className='mt-2 flex justify-end'>
-                        <Button 
+                        <Button
                             type="submit"
                             disabled={loading}>
                             {loading ? <LoaderCircle className='animate-spin' /> : "Save"}
